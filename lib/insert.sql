d20c3d3a3cac129f746293637f7c61b6d32a66fd
Everlyn <gachuieve32@gmail.com>
2023-12-01 20:32:45 +0300

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Mr. Chocolate', 20, 'M', 'brown', 'phlegmatic', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Rowdy', 30, 'M', 'black', 'phlegmatic', 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Tabitha', 25, 'F', 'white', 'sanguine', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Sergeant Brown', 11, 'M', 'brown', 'choleric', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Melissa', 38, 'F', 'white', 'sanguine', 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Grinch', 70, 'M', 'brown', 'choleric', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Wendy', 100, 'F', 'black', 'melancholic', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(NULL, 40, 'F', 'chocolate', 'sanguine', 0)
